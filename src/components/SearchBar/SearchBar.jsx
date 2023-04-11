d041ca16d647066e4efdbd9ba505dfdeca3bdd30
Luiz Neto <100620915+LuizFNeto20@users.noreply.github.com>
2023-04-11 16:54:27 -0400

    Login ajeitado, e header finalizado

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,18 @@
-import './index.scss';
+import logoCarrinho from '../../assets/imgs/cart-icon.png'
+import iconeInput from '../../assets/imgs/search-icon.png'
 
-const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
+function Search() {
+
+    return (
+        <>
+            <div className='header__div'>
+                <img src={iconeInput} alt="" className='header__icon'/>
+                <input className='header__input' type="text" placeholder='O que você está procurando?' />
+            </div>
+            <img src={logoCarrinho} alt=""  className='header__cart'/>
+        </>
+    )
+
 }
 
-export default SearchBar
+export default Search;
