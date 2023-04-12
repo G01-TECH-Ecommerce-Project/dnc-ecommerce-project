31e4c5c638a922c69b1ce580d0203ecdd109c3dd
Victor José <112560183+VictorJoseHMacedo@users.noreply.github.com>
2023-04-12 13:42:49 -0300

    criacao de banner automatico

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,18 @@
 import './index.scss';
 import HeaderMenuDetail from '../../components/HeaderMenuDetail/HeaderMenuDetail';
 import Footer from '../../components/Footer/Footer';
-import Banner from '../../assets/imgs/home-banner.png';
+import Banner from '../../components/Banner/Banner';
+
 
 const Home = ({data}) => {
   return (
       <>
         <HeaderMenuDetail/>
-        <div className='home'>
-          <div className="home__banner">
-            <img src={Banner} alt="Banner Playstation" />
-          </div>
+        <Banner />
+          
           <div className="home__products">
           </div>
-        </div>
+
         <Footer/>
       </>
   )
