acec1bb09573ab6929dd6cd80181bbfe1881675c
Thiago Lemos <thiago_lemosbarbosa@hotmail.com>
2023-04-12 18:43:07 -0300

    criação dos cards

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,20 +2,22 @@ import './index.scss';
 import HeaderMenuDetail from '../../components/HeaderMenuDetail/HeaderMenuDetail';
 import Footer from '../../components/Footer/Footer';
 import Banner from '../../assets/imgs/home-banner.png';
+import ProductCard from '../../components/ProductCard/ProductCard';
 
-const Home = ({data}) => {
+const Home = ({ data }) => {
   return (
-      <>
-        <HeaderMenuDetail/>
-        <div className='home'>
-          <div className="home__banner">
-            <img src={Banner} alt="Banner Playstation" />
-          </div>
-          <div className="home__products">
-          </div>
+    <>
+      <HeaderMenuDetail />
+      <div className='home'>
+        <div className="home__banner">
+          <img src={Banner} alt="Banner Playstation" />
         </div>
-        <Footer/>
-      </>
+      </div>
+      <div className="home__productCard">
+        {data.map(product => (<ProductCard key={product.id} data={product} />))}
+      </div>
+      <Footer />
+    </>
   )
 }
 
